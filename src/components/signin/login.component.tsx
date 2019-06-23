7153fb8f7aa051aeead30b54e870bc9673d22474
Elias Friedman <efriedman02@gmail.com>
2019-06-23 15:01:15 -0400

    made registration look a lot nicer

--- src/components/signin/login.component.tsx
+++ src/components/signin/login.component.tsx
@@ -4,15 +4,16 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { IState } from '../../reducers';
 import { connect } from 'react-redux';
 import { sendLogin } from '../../actions/login.action'
+import { Form, FormGroup, Input, Card, Container, Row, Col, Label } from 'reactstrap';
 
 interface ILoginProps extends RouteComponentProps {
   self: any
-  sendLogin: (username:string, password:string, history:any) => void
+  sendLogin: (username: string, password: string, history: any) => void
 }
 
 interface ILoginState {
-  username:string
-  password:string
+  username: string
+  password: string
 }
 
 class LoginForm extends Component<ILoginProps, ILoginState> {
@@ -20,14 +21,14 @@ class LoginForm extends Component<ILoginProps, ILoginState> {
   state = {
     username: '',
     password: ''
-}
+  }
 
   handleSubmit = event => {
     event.preventDefault();
     this.props.sendLogin(this.state.username, this.state.password, this.props.history)
   }
 
-  handleChange = (key:string) => event => {
+  handleChange = (key: string) => event => {
     this.setState({
       ...this.state,
       [key]: event.target.value
@@ -36,18 +37,28 @@ class LoginForm extends Component<ILoginProps, ILoginState> {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="username" onChange={this.handleChange('username')} />
-          <input type="text" placeholder="password" onChange={this.handleChange('password')} />
-          <input type="submit" value="Sign In" />
-        </form>
-      </div>
+      <Container>
+        <Row>
+          <Col xs="4">
+            <Card>
+              <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                  <Label for="username">Username</Label>
+                  <Input type="text" placeholder="username" onChange={this.handleChange('username')} id="username" />
+                  <Label for="password">Password</Label>
+                  <Input type="text" placeholder="password" onChange={this.handleChange('password')} id="password" />
+                </FormGroup>
+                <Input type="submit" value="Sign In" />
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
 
-const mapStateToProps = (state:IState) => ({
+const mapStateToProps = (state: IState) => ({
   self: state.CurrentUser.self
 })
 
